c2db85aa880b5b43e3814f6ea9e0ec00e8a2fc20
ReeceTredwell <Reece.tredwell@students.mq.edu.au>
2024-09-03 09:52:43 +1000

    added Main File, CCP variables defined

--- Carriage Control Program/src/CCP.java
+++ Carriage Control Program/src/CCP.java
@@ -2,7 +2,27 @@ import java.net.InetAddress;
 
 public class CCP{
     InetAddress mcpIP;
-    InetAddress mcpPort;
+    int MCPPort;
     int carriageIP;
     int carriagePort;
+    InetAddress CEPIP;
+    MCP_COMMS_thread MCP;
+    CEP_COMMS_thread CEP;
+
+public CCP(InetAddress mcpIP, int carriageIP, int carriagePort, int MCPPort, InetAddress CEPIP, MCP_COMMS_thread MCP, CEP_COMMS_thread CEP){
+this.mcpIP = mcpIP;
+this.MCPPort = MCPPort;
+this.carriageIP = carriageIP;
+this.carriagePort = carriagePort;
+this.CEPIP=CEPIP;
+this.MCP=MCP;
+this.CEP = CEP;
+}
+public void execute(){
+    //This is where all events will be called,
+    //it will contain a loop that will alwaysw be
+    //true until it is called to stop.
+    //this method will be called in main
+}
+
 }
