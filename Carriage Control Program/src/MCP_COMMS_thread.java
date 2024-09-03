c04c844adfdf5e22170e8f4972075e8997fe758c
Mohammad123nazimuddin <126745259+Mohammad123nazimuddin@users.noreply.github.com>
2024-09-03 12:12:27 +1000

    changed

--- Carriage Control Program/src/MCP_COMMS_thread.java
+++ Carriage Control Program/src/MCP_COMMS_thread.java
@@ -1,12 +1,17 @@
 import java.net.InetAddress;
 import java.nio.Buffer;
+import java.nio.ByteBuffer;
+
 import org.json.simple.JSONObject;
 
 
-
 public class MCP_COMMS_thread extends COMMS_thread {
-    
+   
     public JSONObject getDataFromMCP(){
+       ByteBuffer buffer = new ByteBuffer(100);
         return null;
     };  
+
+    
 }
+
