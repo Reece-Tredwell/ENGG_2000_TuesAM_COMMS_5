2506913fb9bb00600b55403d551edbe4bb229c2e
Mohammad123nazimuddin <126745259+Mohammad123nazimuddin@users.noreply.github.com>
2024-09-03 12:22:06 +1000

    fix buffer

--- Carriage Control Program/src/MCP_COMMS_thread.java
+++ Carriage Control Program/src/MCP_COMMS_thread.java
@@ -6,12 +6,8 @@ import org.json.simple.JSONObject;
 
 
 public class MCP_COMMS_thread extends COMMS_thread {
-<<<<<<< HEAD
-   
-=======
->>>>>>> 220b5304e0442f146536a055232a67d52ce9231c
     public JSONObject getDataFromMCP(){
-       ByteBuffer buffer = new ByteBuffer(100);
+       ByteBuffer buffer = ByteBuffer.allocate(100);
         return null;
     };  
 
