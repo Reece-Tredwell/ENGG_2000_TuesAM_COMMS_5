62ead2526b7d4400920f0ccc72eadb26bef7e5f4
Frank Karae <frankjkarae@gmail.com>
2024-09-03 10:35:31 +1000

    Update CCP.java
    Mistakenly deleted this part

--- Carriage Control Program/src/CCP.java
+++ Carriage Control Program/src/CCP.java
@@ -5,8 +5,24 @@ public class CCP{
     int MCPPort;
     int carriageIP;
     int carriagePort;
+    InetAddress CEPIP;
+    MCP_COMMS_thread MCP;
+    CEP_COMMS_thread CEP;
 
-    public static void execute(){
-        //Do something
-    }
+public CCP(InetAddress mcpIP, int carriageIP, int carriagePort, int MCPPort, InetAddress CEPIP, MCP_COMMS_thread MCP, CEP_COMMS_thread CEP){
+this.mcpIP = mcpIP;
+this.MCPPort = MCPPort;
+this.carriageIP = carriageIP;
+this.carriagePort = carriagePort;
+this.CEPIP=CEPIP;
+this.MCP=MCP;
+this.CEP = CEP;
+}
+public void execute(){
+    //This is where all events will be called,
+    //it will contain a loop that will alwaysw be
+    //true until it is called to stop.
+    //this method will be called in main
+}
+
 }
