09a3f8999ad43069e807467a43eea3a1a5df0bbe
ReeceTredwell <Reece.tredwell@students.mq.edu.au>
2024-09-10 11:04:04 +1000

    new method for UDP

--- Carriage Control Program/src/MCP_COMMS_thread.java
+++ Carriage Control Program/src/MCP_COMMS_thread.java
@@ -1,14 +1,16 @@
 import java.net.InetAddress;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
-
 import org.json.simple.JSONObject;
 
 
-public class MCP_COMMS_thread extends COMMS_thread {
+public class MCP_COMMS_thread{
     public JSONObject getDataFromMCP(){
-       ByteBuffer buffer = ByteBuffer.allocate(100);
-        return null;
+    //needs to establish connection to the MCP and get the data
+        JSONObject TestObject = new JSONObject();
+        TestObject.put("Test", "Hello");
+        System.out.println(TestObject);    
+        return TestObject;
     };  
 
     
