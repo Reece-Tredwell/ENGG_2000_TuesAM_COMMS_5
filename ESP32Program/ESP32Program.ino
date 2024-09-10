a8e87553c8d32b778bb415c996d7db3be26fc1e8
Joshua Usi <joshua.usi2017@gmail.com>
2024-09-10 10:34:57 +1000

    Added command parsing

--- ESP32Program/ESP32Program.ino
+++ ESP32Program/ESP32Program.ino
@@ -8,7 +8,8 @@ void setup() {
   Serial.begin(115200);
   delay(10);
 
-  if (cep.connect(CREDENTIALS_SSID, CCP_IP, CCP_PORT) != CEP::ErrorCode::SUCCESSFUL) exit(-1);
+  if (cep.connect(CREDENTIALS_SSID, CCP_IP, CCP_PORT) != CEP::ErrorCode::SUCCESSFUL)
+    exit(-1);
 }
 
 void loop() {
