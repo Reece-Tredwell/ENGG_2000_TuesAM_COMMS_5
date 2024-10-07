08788a8fde733d05601ad4e11abeea1be34bde4d
Frank Karae <frankjkarae@gmail.com>
2024-10-08 09:46:56 +1100

    Update CCP.java

--- Carriage Control Program/src/CCP.java
+++ Carriage Control Program/src/CCP.java
@@ -10,15 +10,18 @@ public class CCP{
     MCP_COMMS MCP;
     CEP_COMMS CEP;
 
-
+//Initilization
 public CCP(){
 }
+
+//Method
 public void execute(){
     /*/
     This is where all events will be called, it will contain a loop that will always be true until it is called to stop. 
     This method will be called in main
     /*/
     while(true){
+        //
         JSONObject TestObject = new JSONObject();
         TestObject.put("client_type", "ccp");
         TestObject.put("message", "EXEC");
@@ -26,13 +29,14 @@ public void execute(){
         TestObject.put("client_type", "ccp");
         TestObject.put("timestamp","2019-09-07T15:50+00Z");
         TestObject.put("action","SLOW");
+
         CCPMainProcess Main = new CCPMainProcess();
         String IP = "10.20.30.110";
         int MCPPort = 3010;
         JSONObject message = Main.convertToCommand(TestObject);
         CEP_COMMS CEP = new CEP_COMMS(message, IP, MCPPort);
         CEP.writeToESP32();
-}
+    }
 }
 
 }
