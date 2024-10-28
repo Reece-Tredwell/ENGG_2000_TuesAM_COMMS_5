7f94351592824abd000d705f41d4cd999816711a
Frank Karae <frankjkarae@gmail.com>
2024-10-29 09:12:30 +1100

    fdfs

---
+++ Carriage Control Program/src/CCPTest.java
@@ -0,0 +1,8 @@
+// import org.junit.*;
+// public class CCPTest    {
+//     @Test
+//     public void test () {
+//         CCP joe = new CCP();
+//         Assert.assertEquals(joe, joe.sendMessageToCEP());
+//     }
+// }
